60696cf401a527c63308594eb0eef59015d6814a
Kingz245 <80926790+Kingz245@users.noreply.github.com>
2021-03-22 21:35:56 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -32,33 +32,16 @@ let Bogey: Sprite = null
 let projectile: Sprite = null
 let SpacePlane: Sprite = null
 scene.setBackgroundColor(15)
-SpacePlane = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . 6 6 6 6 . . . . . . . . . 
-    . . . 6 8 8 6 6 . . . . . . . . 
-    . . . 6 6 8 8 6 6 6 . . . . . . 
-    . . . . 6 6 8 8 8 6 6 . . . . . 
-    . . a a a a a a a a a a a a . . 
-    . 2 a a 9 9 9 9 2 2 2 2 2 a 7 . 
-    5 2 a a 9 9 9 9 2 6 6 6 2 a 7 7 
-    5 2 a a 9 9 9 9 2 2 2 2 2 a 7 7 
-    5 2 a a 9 9 9 9 9 9 9 9 9 a 7 7 
-    . 2 a a 9 9 9 9 9 9 9 9 9 a 7 . 
-    . . a a a a a a a a a a a a . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+SpacePlane = sprites.create(assets.image`galgaPlane1`, SpriteKind.Player)
 controller.moveSprite(SpacePlane, 200, 200)
 SpacePlane.setStayInScreen(true)
 info.setLife(5)
+if (SpacePlane.vx < 200) {
+    SpacePlane.image.flipX()
+}
 game.onUpdateInterval(1000, function () {
-    Bogey = sprites.create(assets.image`Spider0`, SpriteKind.Enemy)
+    Bogey = sprites.create(assets.image`galgaEnemy1`, SpriteKind.Enemy)
     Bogey.setVelocity(-100, 0)
     Bogey.setPosition(160, randint(5, 115))
     Bogey.setFlag(SpriteFlag.AutoDestroy, true)
-    if (Bogey.vx < 0) {
-        Bogey.image.flipX()
-    }
 })
