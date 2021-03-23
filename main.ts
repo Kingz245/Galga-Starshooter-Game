fdb914fa67d57a542cad093ce6dadb5df7d71695
Kingz245 <80926790+Kingz245@users.noreply.github.com>
2021-03-22 21:30:47 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -32,29 +32,29 @@ let Bogey: Sprite = null
 let projectile: Sprite = null
 let SpacePlane: Sprite = null
 scene.setBackgroundColor(15)
-SpacePlane = sprites.create(assets.image`BlueBaumer`, SpriteKind.Player)
+SpacePlane = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . 6 6 6 6 . . . . . . . . . 
+    . . . 6 8 8 6 6 . . . . . . . . 
+    . . . 6 6 8 8 6 6 6 . . . . . . 
+    . . . . 6 6 8 8 8 6 6 . . . . . 
+    . . a a a a a a a a a a a a . . 
+    . 2 a a 9 9 9 9 2 2 2 2 2 a 7 . 
+    5 2 a a 9 9 9 9 2 6 6 6 2 a 7 7 
+    5 2 a a 9 9 9 9 2 2 2 2 2 a 7 7 
+    5 2 a a 9 9 9 9 9 9 9 9 9 a 7 7 
+    . 2 a a 9 9 9 9 9 9 9 9 9 a 7 . 
+    . . a a a a a a a a a a a a . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
 controller.moveSprite(SpacePlane, 200, 200)
 SpacePlane.setStayInScreen(true)
 info.setLife(5)
 game.onUpdateInterval(1000, function () {
-    Bogey = sprites.create(img`
-        . . . 8 8 . . . . . . . . . . . 
-        . . . 8 f 8 . . . . . . . . . . 
-        . . . 8 f f 8 . . . . . . . . . 
-        . . . 8 f f f f 8 . . . . . . . 
-        . . . 8 8 8 8 8 8 8 . . . . . . 
-        5 f 2 2 2 2 2 2 2 2 2 2 2 2 8 . 
-        5 f 2 2 6 6 6 6 6 6 6 6 2 2 8 8 
-        5 f 2 2 6 9 9 9 9 9 9 9 2 2 8 8 
-        5 f 2 2 6 6 6 6 6 6 6 6 2 2 8 8 
-        5 f 2 2 2 2 2 2 2 2 8 8 2 2 8 8 
-        5 f 2 2 2 2 2 2 2 2 2 2 2 2 8 . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Enemy)
+    Bogey = sprites.create(assets.image`Spider0`, SpriteKind.Enemy)
     Bogey.setVelocity(-100, 0)
     Bogey.setPosition(160, randint(5, 115))
     Bogey.setFlag(SpriteFlag.AutoDestroy, true)
